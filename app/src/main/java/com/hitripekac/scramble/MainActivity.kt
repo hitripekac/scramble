c6ed91250d720ad517a3dc33a40f3ee834d3be44
Igor Janošev <igor.janosev@united.cloud>
2019-08-28 15:02:54 +0200

    Usability changes

--- app/src/main/java/com/hitripekac/scramble/MainActivity.kt
+++ app/src/main/java/com/hitripekac/scramble/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.hitripekac.scramble
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.logging.Logger
+
 
 class ChangeWatcher(private val runner: (String) -> Unit) : TextWatcher {
     override fun afterTextChanged(p0: Editable?) = Unit
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?
 
         decodedCoordinates.addTextChangedListener(ChangeWatcher { coordinates ->
             logger.info("input: $coordinates")
@@ -31,6 +36,13 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        decodedCoordinatesContainer.setEndIconOnClickListener {
+            val data = ClipData.newPlainText("decodedCoordinates", decodedCoordinates.text)
+            myClipboard?.setPrimaryClip(data)
+            Toast.makeText(this, getString(R.string.decoded_coordinates_copied), Toast.LENGTH_SHORT).show()
+
+        }
+
         encodedCoordinates.addTextChangedListener(ChangeWatcher { coordinates ->
             logger.info("input: $coordinates")
             val decoded = coder.decodeCoordinate(coordinates, cipher)
@@ -38,14 +50,14 @@ class MainActivity : AppCompatActivity() {
                 decodedCoordinates.setText(decoded)
             }
         })
+
+        encodedCoordinatesContainer.setEndIconOnClickListener {
+            val data = ClipData.newPlainText("encodedCoordinates", encodedCoordinates.text)
+            myClipboard?.setPrimaryClip(data)
+            Toast.makeText(this, getString(R.string.encoded_coordinates_copied), Toast.LENGTH_SHORT).show()
+        }
     }
 
-//    fun resultClickHandle(_view: View) {
-//        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-//        val clip = ClipData.newPlainText("coordinates", resultText.text)
-//        clipboard.setPrimaryClip(clip)
-//        Toast.makeText(this, R.string.result_copied, Toast.LENGTH_SHORT).show()
-//    }
 
     private val cipher get() = cipherSelect.selectedItemPosition
 
